91611b5d04fe6738399a88fa512e44d0a32db51c
Matheus-Novoa <65021130+Matheus-Novoa@users.noreply.github.com>
2026-07-09 16:10:27 -0300

    fix: correcao na silver_empresas

--- dbt_reciclagem/models/silver/silver_empresas.sql
+++ dbt_reciclagem/models/silver/silver_empresas.sql
@@ -5,5 +5,5 @@ select
     cast(nullif(trim(qualificacao_responsavel), '') as integer) as qualificacao_responsavel,
     cast(replace(nullif(trim(capital_social), ''), ',', '.') as decimal(18, 2)) as capital_social,
     cast(nullif(trim(porte_empresa), '') as integer) as porte_empresa,
-    nullif(trim(cnpj_basico), '') as cnpj_basico
+    nullif(trim(ente_federativo), '') as ente_federativo
 from {{ ref('bronze_empresas') }}
